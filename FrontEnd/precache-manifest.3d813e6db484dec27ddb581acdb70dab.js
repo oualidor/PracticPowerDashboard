6a62860681cf9fa15414e731e0c2a895807215e6
oualidor <walid.khial@gmail.com>
2021-12-14 16:50:29 +0100

    Hi

---
+++ FrontEnd/precache-manifest.3d813e6db484dec27ddb581acdb70dab.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a62812571352ef9bcc87504ae1c93b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb9996fb519c8a2592d6",
+    "url": "/static/css/2.4626ed4d.chunk.css"
+  },
+  {
+    "revision": "f843bae776660e47361b",
+    "url": "/static/css/main.7002013f.chunk.css"
+  },
+  {
+    "revision": "eb9996fb519c8a2592d6",
+    "url": "/static/js/2.4d4cb5d3.chunk.js"
+  },
+  {
+    "revision": "00d78e29ab8daaf0e27586a4b4ae9863",
+    "url": "/static/js/2.4d4cb5d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f843bae776660e47361b",
+    "url": "/static/js/main.29c0b887.chunk.js"
+  },
+  {
+    "revision": "a1e817fecf0c02e4b09a",
+    "url": "/static/js/runtime-main.353383e3.js"
+  },
+  {
+    "revision": "5c57343daaa5ad12d75a54138d9a5333",
+    "url": "/static/media/IPhone7.5c57343d.png"
+  },
+  {
+    "revision": "bff0499a02e735ab2f1d1b0cedf2f66c",
+    "url": "/static/media/ipad.bff0499a.jpg"
+  },
+  {
+    "revision": "2673b52f89c00e1abffca5daf1813b7a",
+    "url": "/static/media/logo.2673b52f.svg"
+  },
+  {
+    "revision": "89c4cb303670dcc4662cde24cc628ac8",
+    "url": "/static/media/mobile.89c4cb30.svg"
+  },
+  {
+    "revision": "b2922fea13ddcd5ea91241103f8907e9",
+    "url": "/static/media/rotate.b2922fea.svg"
+  },
+  {
+    "revision": "39131b70da919c90c7313d889e656e16",
+    "url": "/static/media/tablet.39131b70.svg"
+  }
+]);
--- FrontEnd/service-worker.js
+++ FrontEnd/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.54fe3d8b24dd7ffb92c8aa338f37002e.js"
+  "/precache-manifest.3d813e6db484dec27ddb581acdb70dab.js"
 );
 
 self.addEventListener('message', (event) => {
